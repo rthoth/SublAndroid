d886106b4dd7355f7b1ed1ecb85ec20606bec3d7
rthoth <ronaldo.silva@lemaf.ufla.br>
2015-06-27 10:20:48 -0300

    Daemon more robust(????)

--- lib/daemon.py
+++ lib/daemon.py
@@ -26,10 +26,12 @@ class Daemon(Emitter):
         self._port = port()
         self._popen = Popen([daemon_command, project_path, str(self._port), 'debug'])
         self._attempt = 0
+        self._off = False
         sublime.set_timeout_async(self._try_connect, _ATTEMPT_DELAY)
 
     def shutdown(self):
-        pass
+        self._off = True
+        self._popen.terminate()
 
     def _connect(self):
         self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -45,6 +47,8 @@ class Daemon(Emitter):
             except ConnectionRefusedError:
                 if self._attempt <= _MAX_ATTEMPTS:
                     sublime.set_timeout_async(self._try_connect, _ATTEMPT_DELAY)
+                else:
+                    self.fire('start_failed')
         else:
             self._socket = False
 
@@ -78,12 +82,24 @@ class Daemon(Emitter):
                     return
 
             json = data.decode()
-            sublime.message_dialog('Gradle Messagem: ' + json + '.')
-            json = sublime.decode_value(json)
-            callback(None, json)
+            sublime.message_dialog('Gradle Response: %s.' % json)
+            status = json[0]
+
+            try:
+                message = sublime.decode_value(json[1:])
+            except ValueError as value_error:
+                message = value_error
+
+            if status == 'S':
+                callback(None, message)
+            else:
+                callback(message)
+
         elif self._socket is None:
             sublime.set_timeout_async(partial(self._send, message, callback), _ATTEMPT_DELAY)
 
     def wait(self):
         self._popen.wait()
         self.fire('end')
+        if not self._off:
+            self.fire('crash')
